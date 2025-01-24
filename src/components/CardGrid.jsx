079ec55ff6d70f2d412953382598b30f3d6a452d
tanishk <tanishk2250@gmail.com>
2025-01-24 17:25:30 +0530

    polish ui

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -20,7 +20,27 @@ const Card = ({ gif, title, likes, views, link }) => {
 const CardGrid = () => {
   const cards = [
     {
-      gif: 'https://content.instructables.com/FBS/FJTQ/M5V22LY4/FBSFJTQM5V22LY4.jpg?auto=webp&frame=1&width=734&height=1024&fit=bounds&md=MjAyNS0wMS0xMyAxNTo1Mjo0NC4w',
+      gif: 'https://content.instructables.com/F7B/TIKD/M5V22GKL/F7BTIKDM5V22GKL.jpg?auto=webp&frame=1&width=1024&fit=bounds&md=MjAyNS0wMS0xMyAxNDo1Mjo1NS4w',
+      title: 'Bird Like Ornithopter Mechanism',
+      link: '/techs/bird-like-ornithopter-mechanism/',
+    },
+    {
+      gif: 'https://content.instructables.com/F7B/TIKD/M5V22GKL/F7BTIKDM5V22GKL.jpg?auto=webp&frame=1&width=1024&fit=bounds&md=MjAyNS0wMS0xMyAxNDo1Mjo1NS4w',
+      title: 'Bird Like Ornithopter Mechanism',
+      link: '/techs/bird-like-ornithopter-mechanism/',
+    },
+    {
+      gif: 'https://content.instructables.com/F7B/TIKD/M5V22GKL/F7BTIKDM5V22GKL.jpg?auto=webp&frame=1&width=1024&fit=bounds&md=MjAyNS0wMS0xMyAxNDo1Mjo1NS4w',
+      title: 'Bird Like Ornithopter Mechanism',
+      link: '/techs/bird-like-ornithopter-mechanism/',
+    },
+    {
+      gif: 'https://content.instructables.com/F7B/TIKD/M5V22GKL/F7BTIKDM5V22GKL.jpg?auto=webp&frame=1&width=1024&fit=bounds&md=MjAyNS0wMS0xMyAxNDo1Mjo1NS4w',
+      title: 'Bird Like Ornithopter Mechanism',
+      link: '/techs/bird-like-ornithopter-mechanism/',
+    },
+    {
+      gif: 'https://content.instructables.com/F7B/TIKD/M5V22GKL/F7BTIKDM5V22GKL.jpg?auto=webp&frame=1&width=1024&fit=bounds&md=MjAyNS0wMS0xMyAxNDo1Mjo1NS4w',
       title: 'Bird Like Ornithopter Mechanism',
       link: '/techs/bird-like-ornithopter-mechanism/',
     },
